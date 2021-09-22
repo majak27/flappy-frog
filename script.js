d6fe187c45cbdda8653928aefd8e46a92237d479
Maja Kersten <74767271+majak27@users.noreply.github.com>
2021-09-22 10:39:28 +0000

    gaat omhoog

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+let x = 250;
+let y = 250;
+
 function setup() {
 	createCanvas(500, 500);
   background(50);
 }
 
 function draw() {
-	ellipse(250,250,50,50)
-  
+	ellipse(x,y,50,50)
+  if (key===" "){
+    y = y - 10
+  }
 }
 
