7af1279b4310f46e43db21855d878213bb7d7140
Maja Kersten <74767271+majak27@users.noreply.github.com>
2021-10-04 13:35:11 +0000

    1 pijp toegevoegd

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
+let pipes = [];
 var gravity = 0.25;
 var y = 200;
 var vy = -1.0;
 var frog;
 
+function preload() {
+  bg = loadImage('pictures/ass.jpg');
+  ps = loadImage('pictures/paddenstoel.png');
+}
 
 class Paddenstoel{
-  constructor(bottom, height,v){
-    this.bottom = bottom;
-    this.height = height;
-    this.v = v;
+  constructor(x, y, w, h){
+    this.x = x;
+    this.y= y;
+    this.w = 50;
+    this.h = random(100,155);
   }
+  drawPipe(){
+    fill(255);
+    rect(this.x, 0, this.w, this.h);
+  }
+
 }
 
 class Frog{
@@ -20,13 +31,12 @@ class Frog{
   }
 
   draw(){
-    ellipse(100, this.y, 60, 60);
+    ellipse(150, this.y, 60, 60);
   }
-  move(){
+
+  move(){   
     this.vy += this.gravity;
     this.y += this.vy;
-
-    console.log(this.vy)
 
     this.y = constrain(this.y, 30, 370);
   } 
@@ -39,16 +49,17 @@ function keyPressed(){
 }
 
 function setup() {
-  createCanvas(500, 400);
-  bg = loadImage("ass.jpg");
+  createCanvas(550, 400);
   frog = new Frog(-0.5, 150, 0.25);
+  var pipe = new Paddenstoel(50,50,50,50);
+  pipes.push(pipe);
 }
 
 function draw() {
   background(bg);
   frog.draw();
   frog.move();
+  for (var i = pipes.length-1; i >= 0; i--) {
+    pipes[i].drawPipe();
+  }
 }
-
-
-
