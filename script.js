1c465f329417a9e03fae5e2b9ccc83244a51442a
Maja Kersten <74767271+majak27@users.noreply.github.com>
2021-10-05 15:54:32 +0000

    bewegende pipes

--- script.js
+++ script.js
@@ -14,13 +14,15 @@ class Paddenstoel{
     this.x = x;
     this.y= y;
     this.w = 50;
-    this.h = random(100,155);
-  }
+    this.h = random(50,180);
+    this.speed = 1;
+    }
   drawPipe(){
-    fill(255);
-    rect(this.x, 0, this.w, this.h);
+    rect(this.x, this.y, this.w, this.h);
   }
-
+  movePipe(){
+    this.x -= this.speed;
+  }
 }
 
 class Frog{
@@ -37,7 +39,6 @@ class Frog{
   move(){   
     this.vy += this.gravity;
     this.y += this.vy;
-
     this.y = constrain(this.y, 30, 370);
   } 
 }
@@ -51,15 +52,18 @@ function keyPressed(){
 function setup() {
   createCanvas(550, 400);
   frog = new Frog(-0.5, 150, 0.25);
-  var pipe = new Paddenstoel(50,50,50,50);
-  pipes.push(pipe);
 }
 
 function draw() {
   background(bg);
   frog.draw();
   frog.move();
+  if (frameCount % 150 == 0) {
+    pipes.push(new Paddenstoel(550,0,this.w,this.h));
+    pipes.push(new Paddenstoel(550,400,this.w, this.h));
+  }
   for (var i = pipes.length-1; i >= 0; i--) {
     pipes[i].drawPipe();
+    pipes[i].movePipe();
   }
 }
