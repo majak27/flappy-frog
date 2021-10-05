8cdbb0c6559bc5cb58e3f0744e902bd61a9d0005
Maja Kersten <74767271+majak27@users.noreply.github.com>
2021-10-05 18:48:57 +0000

    twee rijen pipes en een foto als frog

--- script.js
+++ script.js
@@ -7,18 +7,30 @@ var frog;
 function preload() {
   bg = loadImage('pictures/ass.jpg');
   ps = loadImage('pictures/paddenstoel.png');
+  fr = loadImage('pictures/frog.png')
 }
 
 class Paddenstoel{
-  constructor(x, y, w, h){
+  constructor(x, y, w, h, top, bottom, speed){
     this.x = x;
     this.y= y;
     this.w = 50;
-    this.h = random(50,180);
+    this.h = random(80,140);
+    this.top = (200 + (200-this.h));
+    this.bottom = (200-this.h);
     this.speed = 1;
     }
   drawPipe(){
+    // hoe beide rijen random lengte?
     rect(this.x, this.y, this.w, this.h);
+    rect(this.x, 400 - this.h, this.w, this.h);
+    noStroke();
+    // dit doet t niet voor some reason
+    // if (frog.y < this.top || frog.y > height - this.bottom) {
+    //   if (frog.x > this.x && frog.x < this.x + this.w) {
+    //     fill("red")
+    //   }
+    // }
   }
   movePipe(){
     this.x -= this.speed;
@@ -33,13 +45,14 @@ class Frog{
   }
 
   draw(){
-    ellipse(150, this.y, 60, 60);
+    image(fr, 150, this.y, 60, 60);
   }
 
   move(){   
     this.vy += this.gravity;
     this.y += this.vy;
-    this.y = constrain(this.y, 30, 370);
+    // dit hieronder doet raar met de foto als frog
+    this.y = constrain(this.y, 200, 400);
   } 
 }
 
@@ -58,9 +71,8 @@ function draw() {
   background(bg);
   frog.draw();
   frog.move();
-  if (frameCount % 150 == 0) {
+  if (frameCount % 200 == 0) {
     pipes.push(new Paddenstoel(550,0,this.w,this.h));
-    pipes.push(new Paddenstoel(550,400,this.w, this.h));
   }
   for (var i = pipes.length-1; i >= 0; i--) {
     pipes[i].drawPipe();
